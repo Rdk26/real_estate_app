895b92151627df7c193e62592c9095911f8cb0ef
melvintivane <mtivane@technoplus.co.mz>
2024-05-11 23:18:38 +0200

    build: configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:real_estate_app/firebase_options.dart';
 import 'pages/root.dart';
 import 'theme/color.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
